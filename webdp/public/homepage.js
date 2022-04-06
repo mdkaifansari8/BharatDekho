c9bed880a743b386838f991f31145fe468dbc2e1
thesuaveguy <lit2021026@iiitl.ac.in>
2022-04-06 12:28:54 +0530

    first commit

---
+++ webdp/public/homepage.js
@@ -0,0 +1,21 @@
+import React from "react"
+// import "./homepage.html"
+import "./homepage.css"
+import { useHistory } from "react-router-dom"
+
+const Homepage = () => {
+    const history = useHistory()
+
+    return (
+        <div className="homepage">
+          
+         
+
+            <div className="button" onClick={() => history.push("/login")} >Logout</div>
+        </div>
+        
+    )
+}
+   
+
+export default Homepage
